4fef07562565c96111a86e0adbd8f2862f9e0e27
Md. Ariful Islam <ariful.rony10@gmail.com>
2022-07-07 17:49:37 +0600

    signup page created

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 /**
- * Name: App (The beginning of the program)! 
- * Description: 
+ * Name: App (The beginning of the program)!
+ * Description:
  */
-
-const express = require('express'); 
-const path = require('path'); 
-const app = express(); 
+const express = require('express'); // require express
+const path = require('path'); // builtin path package
+const app = express(); // creating an object of express
 const authenticationRoutes = require('./routes/authentication.routes'); // requiring authentication routes
 
 // Templeting engine
-
+app.set('view engine', 'ejs'); // set up view engine
+app.set('views', path.join(__dirname, 'views')); // pointing to views / where to find the views
 // Middleware
 app.use(authenticationRoutes); // authentication router middleware
-
 
 app.PORT = process.env.PORT || 3000;
 
