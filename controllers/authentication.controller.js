f591e143dbede72b6083b694dd8bf1a9ecd78fc1
Md. Ariful Islam <ariful.rony10@gmail.com>
2022-07-16 12:52:03 +0600

    Handling Errors In Asynchronous Code (authentication.controller.js)

--- controllers/authentication.controller.js
+++ controllers/authentication.controller.js
@@ -3,13 +3,13 @@
  * Description:
  */
 const User = require('../models/user.model');
-const authUtil = require('../util/authentication')
+const authUtil = require('../util/authentication');
 // Get Signup || /signup
 function getSignup(req, res) {
   res.render('customer/authentication/signup');
 }
 //
-async function signup(req, res) {
+async function signup(req, res, next) {
   const user = new User(
     req.body.email,
     req.body.password,
@@ -19,7 +19,12 @@ async function signup(req, res) {
     req.body.city
   );
 
-  await user.signup();
+  try {
+    await user.signup();
+  } catch (error) {
+    next(error);
+    return;
+  }
 
   res.redirect('/login');
 }
@@ -28,30 +33,39 @@ function getLogin(req, res) {
   res.render('customer/authentication/login');
 }
 
-// Logged in a user // /login 
-async function login(req, res) {
+// Logged in a user // /login
+async function login(req, res, next) {
   const user = new User(req.body.email, req.body.password);
-  const existingUser = await user.getUserWithSameEmail();
+  let existingUser;
+  try {
+    existingUser = await user.getUserWithSameEmail();
+  } catch (error) {
+    next(error);
+    return;
+  }
+
   if (!existingUser) {
     res.redirect('/login');
     return;
   }
 
-  const passwordIsCorrect = await user.hasMatchingPassword(existingUser.password);
+  const passwordIsCorrect = await user.hasMatchingPassword(
+    existingUser.password
+  );
 
-  if(!passwordIsCorrect) {
+  if (!passwordIsCorrect) {
     res.redirect('/login');
     return;
   }
 
   authUtil.createUserSession(req, existingUser, function () {
     res.redirect('/');
-  })
-} 
+  });
+}
 
 function logout(req, res) {
   authUtil.destroyUserAuthenticationSession(req);
-  res.redirect('/')
+  res.redirect('/');
 }
 
 // Export Authentication Controllers
